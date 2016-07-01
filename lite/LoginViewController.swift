f6d43bd58954e0d1a3227075dd93c6f4315d3652
苏日甫 <1020343499@qq.com>
2016-07-01 17:40:04 +0800

    手机通讯录
    实现了页面之间的跳转，输入用户名和密码就可以登录到联系人页面。

---
+++ lite/LoginViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LoginViewController.swift
+//  lite
+//
+//  Created by 20141105039 on 16/7/1.
+//  Copyright © 2016年 20141105039. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+          @IBOutlet weak var nameField: UITextField!
+          @IBOutlet weak var pswField: UITextField!
+          @IBOutlet weak var rembSwich: UISwitch!
+          @IBOutlet weak var loginBtn: UIButton!
+          @IBAction func loginAction() {
+          }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+          
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ lite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lite
+//
+//  Created by 20141105039 on 16/7/1.
+//  Copyright © 2016年 20141105039. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+          override func viewDidLoad() {
+                    super.viewDidLoad()
+                    // Do any additional setup after loading the view, typically from a nib.
+          }
+
+          override func didReceiveMemoryWarning() {
+                    super.didReceiveMemoryWarning()
+                    // Dispose of any resources that can be recreated.
+          }
+
+
+}
+
